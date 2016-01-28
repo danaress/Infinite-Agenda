82958cf606b92c15d17eb40940b02f5eb6163589
Student <ressdana@gmail.com>
2016-01-28 15:32:46 -0700

    auto updating dates

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ var myApp = angular.module('myApp', ['infinite-scroll']);
 
 myApp.controller('numbersController', ['$scope', function($scope){
 
+			$scope.d = new Date();
+			$scope.n = d.toString();
     		$scope.counter = 0;
     		$scope.numbers = [];
     $scope.loadMore = function() {
    			
-    
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 1; i++) {
         	var date = {
     		counter : $scope.counter
     		};
