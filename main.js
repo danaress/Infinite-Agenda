717b0f1693a45823eb0ab0418ca22f179201d667
Student <ressdana@gmail.com>
2016-01-29 14:50:52 -0700

    working on saving notes

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ var myApp = angular.module('myApp', ['infinite-scroll']);
 
 myApp.controller('numbersController', ['$scope', function($scope){
 
-		    $scope.today = new Date('2016', '00', '28');
+            $scope.hideNotes = true;
+            $scope.noteButton = true;
+            $scope.saveButton = false;
+            $scope.notes = '';
 
+            $scope.addNote = function(){
+                $scope.hideNotes = false;
+                $scope.noteButton = false;
+                $scope.saveButton = true;
+            }
+
+            $scope.submitNote = function() {
+                $scope.saveButton = false;
+                $scope.noteButton = true;
+                $scope.hideNotes = true;
+            }
+            var notenum = '';
+            $scope.submitNote = function(){
+                $scope.box.notes.push($index);
+            }
+
+		    $scope.today = new Date('2016', '00', '28');
     		$scope.counter = 0;
     		$scope.numbers = [];
     $scope.loadMore = function() {
@@ -13,8 +33,11 @@ myApp.controller('numbersController', ['$scope', function($scope){
         for (var i = 0; i < 1; i++) {
         	var box = {
     		counter : $scope.counter,
-            today : $scope.today
+            today : $scope.today,
+            notes : notenum
     		};
+
+            console.log(box);
             console.log($scope.today.getDate);
             $scope.today.setDate($scope.today.getDate() +1);
         	$scope.counter = $scope.counter + 1;
